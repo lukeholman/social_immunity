1ce9e19446a3c5559528bd2605aef694c373efa0
lukeholman <queen-pheromone-RNAseq>
2020-04-21 17:48:20 +1000

    First commit 2020

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -6,10 +6,16 @@ save_and_display_table <- function(tabl, filename){
 get_fixed_effects_with_p_values <- function(brms_model){
   fixed_effects <- data.frame(summary(brms_model)$fixed) %>%
     rownames_to_column("Parameter")
-  fixed_effects$p <- (100 - as.data.frame(bayestestR::p_direction(brms_model))$pd) / 100
+  p <- as.data.frame(bayestestR::p_direction(brms_model)) %>%
+    filter(Component == "conditional") %>% pull(pd)
+  fixed_effects$p <- 1 - p
   fixed_effects %>% select(Parameter, everything()) %>%
-    rename(lower_95_CI = l.95..CI, upper_95_CI = u.95..CI) %>%
-    mutate(` ` = ifelse(p < 0.05, "\\*", " "))
+    rename(lower_95_CI = l.95..CI, upper_95_CI = u.95..CI)  %>%
+    mutate(` ` = ifelse(p < 0.1, "~", ""),
+           ` ` = replace(` `, p < 0.05, "\\*"),
+           ` ` = replace(` `, p < 0.01, "**"),
+           ` ` = replace(` `, p < 0.001, "***"),
+           ` ` = replace(` `, p == " ", ""))
 }
 
 get_random_effects <- function(brms_model){
@@ -18,3 +24,4 @@ get_random_effects <- function(brms_model){
   random_effects$p <- NA
   random_effects %>% select(Parameter, everything())
 }
+
